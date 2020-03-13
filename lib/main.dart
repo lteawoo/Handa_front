17cf77f6647f049a58183ac23ed12fc7020cb981
Taewoo Lee <lteawoo@naver.com>
2020-03-13 17:12:11 +0900

    Drag 위에서 아래로 구현안됨

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<TodoItemWidget> list = [];
+/*    List<TodoItemWidget> list = [];
 
     list.add(new TodoItemWidget(
       id: 1,
@@ -18,8 +18,26 @@ class MyApp extends StatelessWidget {
       id: 2,
       content: '바나나를 먹자',
       done: true,
+    ));*/
+
+    List<TodoItem> list = [];
+
+    list.add(new TodoItem(
+      no: 1,
+      content: '딸기를 먹자',
+      done: false,
     ));
+
+    list.add(new TodoItem(
+      no: 2,
+      content: '바나나를 먹자',
+      done: true,
+    ));
+
+
+
     debugPrint('list size: ' + list.length.toString());
+
     return MaterialApp(
       title: 'Handa',
       home: Scaffold(
@@ -55,30 +73,50 @@ class TodoItemWidget extends StatefulWidget {
 class _TodoItemState extends State<TodoItemWidget> {
   @override
   Widget build(BuildContext context) {
-    return new Card(
-      child: new Container(
-        padding: new EdgeInsets.all(5.0),
-        child: new Column(
-          children: <Widget>[
-            new CheckboxListTile(
-              value: widget.done,
-              title: new Text(widget.content),
-              controlAffinity: ListTileControlAffinity.leading,
-              onChanged: (bool val) {
-                setState(() {
-                  widget.done = val;
-                });
-              },
+    Card card = new Card(
+        child: new Container(
+            padding: new EdgeInsets.all(5.0),
+            child: new Column(
+              children: <Widget>[
+                new CheckboxListTile(
+                  value: widget.done,
+                  title: new Text(widget.content),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  onChanged: (bool val) {
+                    setState(() {
+                      widget.done = val;
+                    });
+                  },
+                )
+              ],
             )
-          ],
         )
-      )
+    );
+
+    return new LongPressDraggable(
+      child: card,
+      childWhenDragging: Opacity(
+        opacity: 0.5,
+        child: card,
+      ),
+      feedback: Material(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+              maxWidth: MediaQuery
+                  .of(context)
+                  .size
+                  .width
+          ),
+          child: card,
+        ),
+        elevation: 4.0,
+      ),
     );
   }
 }
 
 class TodoItemListWidget extends StatefulWidget {
-  final List<TodoItemWidget> items;
+  final List<TodoItem> items;
   const TodoItemListWidget({
     Key key,
     this.items,
@@ -88,15 +126,104 @@ class TodoItemListWidget extends StatefulWidget {
   _TodoItemListState createState() => _TodoItemListState();
 }
 
-class _TodoItemListState extends State<TodoItemListWidget> {
+class _TodoItemListState extends State<TodoItemListWidget> with TickerProviderStateMixin {
+  Widget buildRow(int index) {
+    final TodoItem item = widget.items[index];
+
+    Card card = new Card(
+        child: new Container(
+            padding: new EdgeInsets.all(5.0),
+            child: new Column(
+              children: <Widget>[
+                new CheckboxListTile(
+                  title: new Text(item.content),
+                  value: item.done,
+                  controlAffinity: ListTileControlAffinity.leading,
+                  onChanged: (bool val) {
+                    setState(() {
+                      item.done = val;
+                    });
+                  },
+                )
+              ],
+            )
+        )
+    );
+
+    Draggable draggable = new LongPressDraggable<TodoItem>(
+      data: item,
+      axis: Axis.vertical,
+      maxSimultaneousDrags: 1,
+      child: card,
+      childWhenDragging: Opacity(
+        opacity: 0.5,
+        child: card,
+      ),
+      feedback: Material(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+              maxWidth: MediaQuery
+                  .of(context)
+                  .size
+                  .width
+          ),
+          child: card,
+        ),
+        elevation: 4.0,
+      ),
+    );
+
+    return DragTarget<TodoItem>(
+      onWillAccept: (item) {
+        return widget.items.indexOf(item) != index;
+      },
+      onAccept: (item) {
+        setState(() {
+          int currentIndex = widget.items.indexOf(item);
+
+          debugPrint(currentIndex.toString());
+
+          widget.items.remove(item);
+          widget.items.insert(currentIndex > index ? index : index - 1, item);
+        });
+      },
+      builder: (BuildContext buildContext, List<TodoItem> candidateData, List<dynamic> rejectedData) {
+        return Column(
+          children: <Widget>[
+            AnimatedSize(
+              duration: Duration(milliseconds: 100),
+              vsync: this,
+              child: candidateData.isEmpty ? Container() : Opacity(
+                opacity: 0.0,
+                child: card,
+              ),
+            ),
+            Container(
+              child: candidateData.isEmpty ? draggable : card,
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return new ListView.builder(
       itemCount: widget.items.length,
-      itemBuilder: (BuildContext context, int index) {
-        return widget.items[index];
-      },
+      itemBuilder: (context, index) => buildRow(index),
     );
   }
+}
+
+class TodoItem {
+  int no;
+  String content;
+  bool done;
+
+  TodoItem({
+    this.no,
+    this.content,
+    this.done,
+  });
 }
