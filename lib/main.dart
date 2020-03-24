51d853a01e5eeae62030b4aa4863a9bdbc66521e
Taewoo Lee <lteawoo@naver.com>
2020-03-24 16:53:58 +0900

    JWT로 authentication 해결해야할듯

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
-
 class TodoItem {
   int no;
   String content;
@@ -38,88 +37,20 @@ class Member {
     this.password,
   });
 
-  Map<String, Map<String, dynamic>> toJson() =>
-      {
-        'email': {
-         'value': email,
-        },
-        'password': {
-         'value': password,
-        },
-      };
+  Map<String, dynamic> toJson() =>
+   {
+     'email': {
+       'value': email,
+     },
+     'password': {
+       'value': password,
+     },
+   };
 }
 
 class MyApp extends StatelessWidget {
-  Future<List<TodoItem>> fetchItem() async {
-    String username = 'test@taeu.kr';
-    String password = '12345';
-    String header = base64Encode(utf8.encode('$username:$password'));
-
-    Member member = new Member(email: username, password: password);
-    debugPrint(member.toJson().toString());
-    debugPrint('json : ' + jsonEncode(<String, dynamic>{
-      'email': {
-       'value': username,
-      },
-       'password': {
-        'value': password,
-      }}));
-//    final response = await http.post(
-//      'http://localhost:8080/member/signin',
-//      headers: {
-//        'Content-Type': 'application/json; charset=UTF-8',
-//        'Accept': '*/*',
-//      },
-//      body: jsonEncode(<String, dynamic>{
-//        'email': {
-//          'value': username,
-//        },
-//        'password': {
-//          'value': password,
-//        },
-//      }),
-//    ).then((value) {
-//      debugPrint(value.body);
-//    }).catchError((error) {
-//      debugPrint(error.toString());
-//    });
-
-    final response = await http.get(
-      'http://localhost:8080/member/test',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
-        'Host': 'localhost:8080',
-      }
-    ).then((value) {
-      debugPrint(value.toString());
-    }).catchError((error) {
-      debugPrint(error.toString());
-    });
-
-    debugPrint('result : ' + response.body);
-    /*final response = await http.get(
-      'http://localhost:8080/api/item/list',
-      *//*headers: {
-        HttpHeaders.authorizationHeader: 'Basic ' + header
-      }*//*
-    ).catchError((error) {
-      debugPrint(error.toString());
-      return error;
-    });*/
-
-    List list = [];
-
-    final responseJson = json.decode(response.body);
-    list = responseJson;
-
-    return list;
-  }
-
   @override
   Widget build(BuildContext context) {
-    fetchItem();
-
     final List<TodoItem> list = [];
     list.add(new TodoItem(
       no: 1,
@@ -172,76 +103,6 @@ class TodoItemListWidget extends StatefulWidget {
 }
 
 class _TodoItemListState extends State<TodoItemListWidget> {
-  void _showModalBottomSheet(BuildContext context) {
-    TextEditingController _controller = TextEditingController();
-
-    submit(String where) {
-      final String input = _controller.text.trim();
-      debugPrint('$where, request submit, you typed $input');
-
-      if(input == '') {
-        debugPrint('$where, you typed blank pass');
-        return;
-      }
-
-      _addTodoItem(new TodoItem(content: input, done: false));
-      debugPrint('submitted, you typed $input');
-    }
-
-    showModalBottomSheet<void>(
-      enableDrag: true,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
-      ),
-      isScrollControlled: true,
-      context: context,
-      builder: (BuildContext context) {
-        return SingleChildScrollView(
-            padding: EdgeInsets.all(10.0),
-            child: Container(
-              padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextField(
-                    textInputAction: TextInputAction.done,
-                    autofocus: true,
-                    minLines: 1,
-                    maxLines: 3,
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      hintText: '무엇을 할건가요?',
-                      border: InputBorder.none,
-                    ),
-                  ),
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      Expanded(
-                        child: Align(
-                          alignment: Alignment.bottomRight,
-                          child: FlatButton.icon(
-                            textColor: Theme.of(context).primaryColor,//Theme.of(context).textTheme.button.color,
-                            icon: const Icon(Icons.add, size: 18),
-                            label: Text('등록'),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            )
-        );
-      },
-    ).whenComplete(() {
-      submit('close');
-    });
-  }
-
   void _addTodoItem(TodoItem todoItem) {
     setState(() {
       widget.items.add(todoItem);
@@ -307,5 +168,128 @@ class _TodoItemListState extends State<TodoItemListWidget> {
         ),
       ),
     );
+  }
+
+  void _showModalBottomSheet(BuildContext context) {
+    TextEditingController _controller = TextEditingController();
+
+    submit(String where) {
+      final String input = _controller.text.trim();
+      debugPrint('$where, request submit, you typed $input');
+
+      if(input == '') {
+        debugPrint('$where, you typed blank pass');
+        return;
+      }
+
+      _addTodoItem(new TodoItem(content: input, done: false));
+      debugPrint('submitted, you typed $input');
+    }
+
+    showModalBottomSheet<void>(
+      enableDrag: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+      ),
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext context) {
+        return SingleChildScrollView(
+            padding: EdgeInsets.all(10.0),
+            child: Container(
+              padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextField(
+                    textInputAction: TextInputAction.done,
+                    autofocus: true,
+                    minLines: 1,
+                    maxLines: 3,
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      hintText: '무엇을 할건가요?',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      /*FlatButton(
+                        child: Text('회원가입'),
+                        onPressed: () {
+                          fetchItem();
+                        },
+                      ),*/
+                      FlatButton(
+                        child: Text('로그인'),
+                        onPressed: () {
+                          signin();
+                        },
+                      ),
+                      FlatButton(
+                        child: Text('아이템목록'),
+                        onPressed: () {
+                          fetchItems();
+                        },
+                      ),
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: FlatButton.icon(
+                            textColor: Theme.of(context).primaryColor,//Theme.of(context).textTheme.button.color,
+                            icon: const Icon(Icons.add, size: 18),
+                            label: Text('등록'),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+        );
+      },
+    ).whenComplete(() {
+      submit('close');
+    });
+  }
+
+  Future<Member> signin() async {
+    String username = 'test@taeu.kr';
+    String password = '12345';
+    String header = base64Encode(utf8.encode('$username:$password'));
+
+    Member member = new Member(email: username, password: password);
+    debugPrint(member.toJson().toString());
+    debugPrint('json : ' + jsonEncode(member));
+    final response = await http.post(
+      'http://localhost:8080/member/signin',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(member),
+    ).catchError((error) {
+      throw error;
+    });
+    debugPrint(response.body);
+    Map<String, String> temp = response.headers;
+    for(String s in temp.keys) {
+      debugPrint(s);
+    }
+    debugPrint(temp.toString());
+  }
+
+  Future<List<TodoItem>> fetchItems() async {
+    final response = await http.post(
+      'http://localhost:8080/api/list',
+    ).catchError((error) {
+      throw error;
+    });
+
+    debugPrint(response.body);
   }
 }
